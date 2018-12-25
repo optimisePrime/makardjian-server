b0cc5603b5a4f7b8b71982a5b7f726f05e709d39
Michael Kardjian <makardjian@gmail.com>
2018-12-24 16:38:34 -0800

    save progress for the day

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -3,10 +3,21 @@ import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './../Components/App.jsx';
 
-Enzyme.configure({ adapter: new EnzymeAdapter() })
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ * @function setup
+ * @constructor
+ * @param {object} props 
+ * @param {any} state 
+ * @returns {ShallowWrapper}
+ */
+const setup = (props={}, state=null) => {
+    return shallow(<App {...props} />)
+}
 
 test('renders the App component without error', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = setup();
     const AppComponent = wrapper.find("[data-test='component-app']")
     expect(AppComponent.length).toBe(1);
 });

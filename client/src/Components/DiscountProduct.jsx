ea8b3a6084df35f9b59721bac62d33ed938ea4d1
Michael Kardjian <makardjian@gmail.com>
2018-12-26 17:54:38 -0800

    final commit on this branch

--- client/src/Components/DiscountProduct.jsx
+++ client/src/Components/DiscountProduct.jsx
@@ -1,45 +1,40 @@
 import React from 'react';
 
   const DiscountProduct = (props) => {
+    const numListPrice = Number(props.product.list_price.slice(1));
+    const numPrice = Number(props.product.price.slice(1));
+    console.log(numListPrice - numPrice);
+    const savings = numListPrice - numPrice;
+    savings = savings.toFixed(2);
     return (
-      <div>
+      <div className='mk-price-div'>
         <table>
           <tbody>
             <tr>
-                <td>
-                    <span>
-                        List Price:
-                    </span>
-                </td>
-                <td>
-                    <span>
-                        {props.product.list_price}
-                    </span>
-                </td>
+              <td>
+                List Price:
+              </td>
+              <td>
+                <span className='mk-text-strike'>
+                  {props.product.list_price}
+                </span>
+              </td>
             </tr>
             <tr>
-                <td>
-                    <span>
-                        Price:
-                    </span>
-                </td>
-                <td>
-                    <span>
-                        {props.product.price}
-                    </span>
-                </td>
+              <td id='mk-price-block'>
+                Price:
+              </td>
+              <td id='mk-discount-price'>
+                {props.product.price}
+              </td>
             </tr>
             <tr>
-                <td>
-                    <span>
-                        You Save:
-                    </span>
-                </td>
-                <td>
-                    <span>
-                        {props.product.discount}
-                    </span>
-                </td>
+              <td>
+                You Save: {savings}
+              </td>
+              <td>
+                {props.product.discount}
+              </td>
             </tr>
           </tbody>
         </table>
